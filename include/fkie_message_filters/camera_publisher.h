03976b722e674a8ed4cc9f7111ed99fb404af3ee
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2020-08-31 13:18:22 +0200

    Rebase from 'release/melodic/fkie_message_filters'

--- include/fkie_message_filters/camera_publisher.h
+++ include/fkie_message_filters/camera_publisher.h
@@ -1,7 +1,7 @@
 /****************************************************************************
  *
  * fkie_message_filters
- * Copyright © 2018 Fraunhofer FKIE
+ * Copyright © 2018-2020 Fraunhofer FKIE
  * Author: Timo Röhling
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,10 +29,11 @@
 namespace fkie_message_filters
 {
 
-/** \brief Publish consumed data to a ROS camera topic
+/** \brief Publish consumed data to ROS camera topics
  *
- * This is a specialized publisher that uses image_transport to publish to a ROS camera topic.
- * All messages which are received from the connected sources will be published on the advertised ROS topic.
+ * This is a specialized publisher that uses image_transport to publish ROS camera topics.
+ * All messages which are received from the connected sources will be published on the corresponding advertised
+ * ROS topics.
  *
  * Unlike regular ROS publishers, this class can be associated with one or more subscriber instances. In that case,
  * the subscribers will subscribe to their ROS topics only if the publisher is actively used. This is a convenient
@@ -80,6 +81,24 @@ public:
      * \nothrow
      */
     void advertise (const image_transport::ImageTransport& it, const std::string& base_topic, uint32_t queue_size, bool latch = false) noexcept;
+    /** \brief Advertise ROS camera topic with subscriber status callbacks.
+     *
+     * All arguments are passed to the ROS client library; see the ROS documentation for further information. Calling
+     * this method will automatically unadvertise any previously advertised ROS topic.
+     *
+     * \arg \c it ROS image_transport instance to handle the publishing
+     * \arg \c base_topic name of the ROS image topic, subject to remapping
+     * \arg \c queue_size size of the ROS publishing queue
+     * \arg \c image_connect_cb callback that is invoked each time a new subscriber connects to the advertised image topic
+     * \arg \c image_disconnect_cb callback that is invoked each time an existing subscriber disconnects from the advertised image topic
+     * \arg \c info_connect_cb callback that is invoked each time a new subscriber connects to the advertised camera info topic
+     * \arg \c info_disconnect_cb callback that is invoked each time an existing subscriber disconnects from the advertised camera info topic
+     * \arg \c tracked_object an associated object whose lifetime will limit the lifetimes of the advertised topics
+     * \arg \c latch if true, the last published message remains available for later subscribers
+     *
+     * \nothrow
+     */
+    void advertise (const image_transport::ImageTransport& it, const std::string& base_topic, uint32_t queue_size, const image_transport::SubscriberStatusCallback& image_connect_cb, const image_transport::SubscriberStatusCallback& image_disconnect_cb = image_transport::SubscriberStatusCallback(), const ros::SubscriberStatusCallback& info_connect_cb = ros::SubscriberStatusCallback(), const ros::SubscriberStatusCallback& info_disconnect_cb = ros::SubscriberStatusCallback(), const ros::VoidPtr& tracked_object = ros::VoidPtr(), bool latch = false) noexcept;
 protected:
     /** \private */
     void receive (const sensor_msgs::ImageConstPtr&, const sensor_msgs::CameraInfoConstPtr&) noexcept override;
